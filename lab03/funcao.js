eaad6f97508174878ca3db464e1c8ae3a7e9797f
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-04-19 17:31:32 -0300

    feat: testa funções anônimas, callback e vetores

--- lab03/funcao.js
+++ lab03/funcao.js
@@ -15,5 +15,33 @@ function somarRetorno(a, b) {
 }
 
 let resultado = somarRetorno(19, 1); 
-console.log("O resultado da soma é: " + resultado);
+console.log("O resultado da soma é: ", resultado);
 
+let somarAnonimo = function(a, b){
+    console.log("O resultado da função anônima é: ", a + b);
+}
+
+somarAnonimo(10, 20); //Função anônima
+
+let somarArrow = (a, b) => a + b;
+
+console.log("O retorno da arrow function é: ", somarArrow(15,35)); //Função arrow
+
+let vetor= [10,20,30,40,50];
+
+function exibirElemento(elemento, indice) {
+    console.log(`Elemento no índice ${indice}: ${elemento}`);
+};
+
+function processarCallback(vetor, callback) {
+    console.log("Processando callback dentro da função forEach");
+    vetor.forEach(callback);
+    
+};
+
+processarCallback(vetor, exibirElemento);
+
+// Callback em arrow function
+vetor.forEach((elemento, indice) => {
+    console.log(`Arrow function exibindo elementos no índice ${indice}: ${elemento} `)
+});
