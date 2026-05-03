56444c13c01f9a952824958e23487ed8991bfe28
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-05-03 09:08:51 -0300

    Feat: Adiciona ajuste no código para conexão com banco de dados sqlite3

--- lab06/index.js
+++ lab06/index.js
@@ -1,40 +1,18 @@
 const express = require('express');
-const fs = require('fs');
+const db = require('./db').db; //Importa a instância do banco de dados
 
 const app = express();
-const arquivo = 'jogos.db';
+const APP_PORT = process.env.APP_PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('API de jogo em execução na porta 3000');
-    console.log('Acesse a url http://localhost:3000');
+//Realiza um parse do body para uma estrutura do JSON
+app.use(express.json());
 
-
-    fs.access(arquivo, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.log('${arquivo} não encontrado. Criando arquivo...');
-            let jogosIniciais = [
-                { id: 1, nome: "Super Mario Wolrd", ano: 1990, categoria: "Plataforma" },
-                { id: 2, nome: "The Legend of Zelda: Ocarina of Time", ano: 1998, categoria: "Ação/Aventura" },
-                { id: 3, nome: "Minecraft", ano: 2011, categoria: "Sandbox" },
-                { id: 4, nome: "The Witcher 3: Wild Hunt", ano: 2015, categoria: "RPG" },
-                { id: 5, nome: "Red Dead Redemption 2", ano: 2018, categoria: "Ação/Aventura" },
-                { id: 6, nome: "God of War", ano: 2018, categoria: "Ação/Aventura" }
-            ];
-            fs.writeFileSync(arquivo, JSON.stringify(jogosIniciais, null, 2));
-        }
-    });
+app.listen(APP_PORT, () => {
+    console.log(`API de jogo em execução na porta ${APP_PORT}`);
+    console.log(`Acesse a url http://localhost:${APP_PORT}`);
 });
 
-app.get('/jogos', (req,res) => {
-    let data = fs.readFileSync(arquivo);
-    let jogos = JSON.parse(data);
-
-    // Verificando se foi passado um parâmetro de busca
-    if (req.query.nome) {
-        jogos = jogos.filter(jogo => jogo.nome.toLowerCase().includes(req.query.nome.toLowerCase()));
-    }
-    res.send(jogos);
-});
+app.get('/', (req,res) => res.send('API Version 1.1.0 on-line!'));
 
 app.get('/jogos/:id', (req,res) => {
     let data = fs.readFileSync(arquivo);
