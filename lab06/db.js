f772f57cd5f61bbfeb25ced3d860747fb7ae8225
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-05-04 21:06:43 -0300

    cria conexão com banco de dados com a variável DB_NAME, método construtor e módulo de exportação

---
+++ lab06/db.js
@@ -0,0 +1,29 @@
+const sqlite3 = require("sqlite3").verbose();
+require('dotenv').config();
+
+class Database {
+    _createTable() {
+        const query = `
+            CREATE TABLE IF NOT EXISTS jogos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                categoria TEXT NOT NULL,
+                ano INTEGER NOT NULL
+            );
+        `;
+        this.db.run(query, (err) => {
+            if (err) console.error("Erro ao criar tabela: ", err.message);
+            else console.log("Tabela 'jogos' verificada/criada");
+        });
+    }
+
+    _connect() {
+        this.db = new sqlite3.Database(process.env.DB_NAME, (err) => {
+            if (err) console.error("Erro ao conectar ao banco de dados: ", err.message);
+            else {
+                console.log("Conexão com o banco de dados estabelecida");
+                this._createTable();
+            }
+        });
+    }
+}
